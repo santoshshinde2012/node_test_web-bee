74f80207f71da271c1585fc88091462b51ee9bb3
Santosh Shinde <santoshshinde2012@gmail.com>
2022-11-27 21:40:24 +0530

    task 4:  Create a migration

--- express_typeorm/src/database/migrations/1663877813247-CinemaSystem.ts
+++ express_typeorm/src/database/migrations/1663877813247-CinemaSystem.ts
@@ -1,6 +1,13 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table
+} from 'typeorm';
 
 export class CinemaSystem1663877813247 implements MigrationInterface {
+
+  name = 'CinemaSystem1663877813247';
+
   /**
    # ToDo: Create a migration that creates all tables for the following user stories
 
@@ -30,9 +37,194 @@ export class CinemaSystem1663877813247 implements MigrationInterface {
    * As a user I want to know where I'm sitting on my ticket
    * As a cinema owner I dont want to configure the seating for every show
    */
-  public async up(queryRunner: QueryRunner): Promise<void> {
-    throw new Error('TODO: implement migration in task 4');
+  public async up(queryRunner: QueryRunner): Promise < void > {
+    // User
+    // Movie
+    // CinemaHall
+    // CinemaSeat
+    // Show
+    // Booking
+    // ShowSeat
+
+    await queryRunner.createTable(
+      new Table({
+        name: "User",
+        columns: [{
+            name: "id",
+            type: "int",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "type",
+            type: 'enum',
+            enum: ['user', 'owner'],
+          }
+        ],
+      }),
+      true,
+    )
+
+    await queryRunner.createTable(
+      new Table({
+        name: "Movie",
+        columns: [{
+            name: "id",
+            type: "int",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+        ],
+      }),
+      true,
+    )
+
+    await queryRunner.createTable(
+      new Table({
+        name: "CinemaHall",
+        columns: [{
+            name: "id",
+            type: "int",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "totalSeat",
+            type: "int",
+          },
+        ],
+      }),
+      true,
+    )
+
+    await queryRunner.createTable(
+      new Table({
+        name: "CinemaSeat",
+        columns: [{
+            name: "id",
+            type: "int",
+            isPrimary: true,
+          },
+          {
+            name: "cinemaHallId",
+            type: "int",
+          },
+          {
+            name: "type",
+            type: "enum",
+            enum: ['couple', 'vip', 'premium'],
+          },
+          {
+            name: "status",
+            type: "enum",
+            enum: ['Available', 'Almost Full', 'Not Available'],
+          }
+        ],
+      }),
+      true,
+    )
+
+    await queryRunner.createTable(
+      new Table({
+        name: "Show",
+        columns: [{
+            name: "id",
+            type: "int",
+            isPrimary: true,
+          },
+          {
+            name: "dateTime",
+            type: "Date",
+          },
+          {
+            name: "startTime",
+            type: "Date",
+          },
+          {
+            name: "endTime",
+            type: "Date",
+          },
+          {
+            name: "movieId",
+            type: "int",
+          },
+          {
+            name: "cinemaHallId",
+            type: "int",
+          }
+        ],
+      }),
+      true,
+    )
+
+    await queryRunner.createTable(
+      new Table({
+        name: "Booking",
+        columns: [{
+            name: "id",
+            type: "int",
+            isPrimary: true,
+          },
+          {
+            name: "dateTime",
+            type: "Date",
+          },
+          {
+            name: "status",
+            type: 'enum',
+            enum: ['passed', 'failed', 'pending'],
+          },
+          {
+            name: "userId",
+            type: "int",
+          },
+          {
+            name: "showId",
+            type: 'int',
+          },
+        ],
+      }),
+      true,
+    )
+
+    await queryRunner.createTable(
+      new Table({
+        name: "ShowSeat",
+        columns: [{
+            name: "id",
+            type: "int",
+            isPrimary: true,
+          },
+          {
+            name: "price",
+            type: "int",
+          },
+          {
+            name: "CinemaSeatId",
+            type: 'int',
+          },
+          {
+            name: "showId",
+            type: 'int',
+          },
+          {
+            name: "bookingId",
+            type: 'int',
+          }
+        ],
+      }),
+      true,
+    )
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise < void > {}
 }
